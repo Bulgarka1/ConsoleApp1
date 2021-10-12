d6ddbb196f849e0974818a23b0483fccd590424b
Ксения Чемлекчиева <your_next_life@mail.ru>
2021-10-12 10:57:11 +0300

    изменен код

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
-namespace Lab_1
+// Пространство имен.
+namespace ConsoleSettings
 {
+    // Класс программы.
     class Program
     {
+        // Метод Main — точка начала выполнения программы.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Информационное табло";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Информация о системе:");
+
+            // Вывод свойств класса Environment.
+            Console.WriteLine(Environment.MachineName);
+            Console.WriteLine(Environment.OSVersion);
+            Console.WriteLine(Environment.UserDomainName);
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine(Environment.Version);
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine(Environment.CurrentManagedThreadId);
+            Console.WriteLine(Environment.ProcessorCount);
+            Console.WriteLine(Environment.Is64BitProcess);
+            Console.WriteLine(Environment.Is64BitOperatingSystem);
         }
     }
 }
+
